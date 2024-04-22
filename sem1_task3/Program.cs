2f189dae7bcf0bdf973b57c926e0600b74b80125
Alina Duhu <metalyugina12@icloud.com>
2024-04-22 12:50:16 +0300

    Добавила задачу3

---
+++ sem1_task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное целое число и на выходе показывает
+// сумму первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
+
+Console.WriteLine("Введите целое трехзначное число:   ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 100 && num <= 999)
+{
+    int firstDigit = num / 100; //456 / 10 == 4
+    int lastDigit = num % 10; // 456 % 10 == 450 + 6
+
+    int result = firstDigit + lastDigit;
+
+    Console.WriteLine($"Сумма первой и последней трехзначного числа = {result}");
+}
+else
+{
+    Console.WriteLine("Ошибка!Число не трехзначное");
+}
